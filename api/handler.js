05ac13d2d7e7a79ce70f6717d319d72152fb7d6f
Mucciano <nicolas.eli.palmucci@gmail.com>
2018-01-05 17:46:44 -0500

    first pass refactor handler to use api functions with async

--- api/handler.js
+++ api/handler.js
@@ -1,40 +1,23 @@
-import AWS from "aws-sdk";
-import zlib from 'zlib';
-import {sendEmail} from './send-email';
 import '../.env';
+import getTemplate from './get-template';
+import { createCampaign, putCampaignHtml, sendCampaignTest } from './chimp-api';
+import {sendEmail} from './send-email';
 
-module.exports.hello = async (event, context, callback) => {
-    const client = new AWS.S3({accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY});
-    const data = new Promise((resolve, reject) => {
-        const downloadStream = client.getObject({Bucket: "id-email", Key: "index.html"}).createReadStream();
-        downloadStream.on('error', error => {
-            reject(console.log('Failed to download', error))
-        });
-        downloadStream.on('data', data => {
-            const buffer = Buffer.from(data);
-            zlib.unzip(buffer, (err, buff) => {
-                if (err) {
-                    console.log('unzip err', err)
-                } else {
-                    console.log('unzip success');
-                    return resolve(buff.toString('utf-8'))
-                }
-            });
-        });
-    });
 
-    const sendRequest = await sendEmail('iirrexample@gmail.com', 'test!', await data);
+module.exports.sendTestEmail = async (event, context, callback) => {
+    const user = process.env.CHIMP_USER;
+    const password = process.env.CHIMP_KEY;
+    const testAddress = process.env.TEST_ADDRESS;
 
-    const response = {
-        status: 200,
-        body: {id: 'fuck'}
-    };
+    const htmlTemplate = await getTemplate('id-email', "index.html");
+    const newCampaign = await createCampaign(user, password);
+    const campaignId = newCampaign.data.id;
 
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-    callback(null, response);
+    const htmlPutResponse = await putCampaignHtml(user, password, campaignId, htmlTemplate);
+    sendCampaignTest(user, password, campaignId, testAddress);
+
+
+    // const sendRequest = await sendEmail('iirrexample@gmail.com', 'new test!', await data);
+
+    callback(null, { message: 'function executed successfully!', event });
 };
-
-
-
-
