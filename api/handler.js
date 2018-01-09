622e806d7c21b8627f4d1b8c9cae49e4eff98b35
Mucciano <nicolas.eli.palmucci@gmail.com>
2018-01-09 04:13:07 -0500

    first pass on control flow for pre-existing vs. new campaigns

--- api/handler.js
+++ api/handler.js
@@ -1,21 +1,60 @@
 import '../.env';
 import getTemplate from './get-template';
 import { createCampaign, putCampaignHtml, sendCampaignTest } from './chimp-api';
-import {sendEmail} from './send-email';
-
+import { getIdFromList } from './check-list';
+import send from './send';
+import {sendEmail} from './send-error';
 
 module.exports.sendTestEmail = async (event, context, callback) => {
+    const bucket = event.Records[0].s3.bucket.name;
     const user = process.env.CHIMP_USER;
     const password = process.env.CHIMP_KEY;
     const testAddress = process.env.TEST_ADDRESS;
 
-    const htmlTemplate = await getTemplate('id-email', "index.html");
-    const newCampaign = await createCampaign(user, password);
-    const campaignId = newCampaign.data.id;
+    const {htmlString, contentId, contentTitle} = await getTemplate(bucket, "index.html");
+    const checkForId = await getIdFromList(user, password, contentId, contentTitle);
+    console.log('Check For ID', checkForId);
 
-    const htmlPutResponse = await putCampaignHtml(user, password, campaignId, htmlTemplate);
-    sendCampaignTest(user, password, campaignId, testAddress);
+    if (checkForId) {
+        const htmlPutToExisting = await putCampaignHtml(user, password, checkForId, htmlString);
 
+        if (!(htmlPutToExisting instanceof Error)) {
+            const campaignId = checkForId;
+
+            const sendConfig = {
+                bucket,
+                user,
+                password,
+                campaignId,
+                testAddress
+            };
+
+            const sendResponse = await send(sendConfig);
+            console.log('Send Branch with pre-existing Campaign');
+        } else {
+            console.log('send aborted pre-existing campagin branch');
+        }
+    } else {
+        const newCampaign = await createCampaign(user, password, contentId, contentTitle);
+        const campaignId = newCampaign.data.id;
+        const htmlPutResponse = await putCampaignHtml(user, password, campaignId, htmlString);
+
+        if (!(htmlPutResponse instanceof Error)) {
+            const sendConfig = {
+                bucket,
+                user,
+                password,
+                campaignId,
+                testAddress
+            };
+
+            const sendResponse = await send(sendConfig);
+            console.log('Send New Campaign Branch');
+        } else {
+            console.log('send aborted new campaign branch');
+        }
+
+    }
 
     // const sendRequest = await sendEmail('iirrexample@gmail.com', 'new test!', await data);
 
